38bd3c19f9a844651f1833a23a801703d8997d9b
ScipioAfricanus1 <bdharris3@uh.edu>
2020-08-31 21:26:41 -0500

    Testing some animation fixes.

--- src/main.rs
+++ src/main.rs
@@ -7,87 +7,46 @@ use bevy::{
 };
 fn main() {
     App::build()
-        .init_resource::<RpgSpriteHandles>()
+        .add_resource(monsterAnimationTimer(Timer::from_seconds(0.05)))
+        .add_resource(characterAnimationTimer(Timer::from_seconds(0.1)))
         .add_default_plugins()
-        .add_startup_system(setup3.system())
+        // .add_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
+        .add_startup_system(setup2.system())
+        .add_system(animate_monster_system.system())
         .add_system(move_sprite_system.system())
         .run();
 }
 
+struct monsterAnimationTimer(Timer);
+struct characterAnimationTimer(Timer);
 
-
-#[derive(Default)]
-pub struct RpgSpriteHandles {
-    handles: Vec<HandleId>,
-    atlas_loaded: bool,
-}
-fn setup3(mut rpg_sprite_handles: ResMut<RpgSpriteHandles>, asset_server: Res<AssetServer>) {
-    rpg_sprite_handles.handles = asset_server
-        .load_asset_folder("assets/textures/rpg")
-        .unwrap();
-}
 
 pub struct Gabe {
     speed: f32,
 }
+pub struct Monster {
+    speed: f32,
+}
 
+
+pub fn animate_monster_system(
+    texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &Monster, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, monster,  mut sprite, texture_atlas_handle) in &mut query.iter() {
+        if timer.finished {
+            let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
+            sprite.index = (sprite.index + 1) % texture_atlas.len() as u32;
+            timer.reset();
+        }
+    }
+}
 pub fn move_sprite_system(
-    mut commands: Commands,
-    mut rpg_sprite_handles:ResMut<RpgSpriteHandles>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    mut textures:ResMut<Assets<Texture>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    texture_atlases: ResMut<Assets<TextureAtlas>>,
     keyboard_input: Res<Input<KeyCode>>,
     time: Res<Time>,
     mut query: Query<(&Gabe, &mut Translation, &mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
 ) {
-    //might need to use this condition on the loading part. We shall see.
-    if rpg_sprite_handles.atlas_loaded {
-        return;
-    }
-
-    let mut texture_atlas_builder = TextureAtlasBuilder::default();
-    if let Some(LoadState::Loaded(_)) =
-        asset_server.get_group_load_state(&rpg_sprite_handles.handles)
-    {
-        for texture_id in rpg_sprite_handles.handles.iter() {
-            let handle = Handle::from_id(*texture_id);
-            let texture = textures.get(&handle).unwrap();
-            texture_atlas_builder.add_texture(handle, &texture);
-        }
-        
-        let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
-        // let texture_atlas_texture = texture_atlas.texture;
-        let vendor_handle = asset_server
-            .get_handle("assets/textures/rpg/chars/gabe/gabe-idle-run-left.png")
-            .unwrap();
-        let texture_atlas_gabe = TextureAtlas::from_grid(vendor_handle, texture_atlas.size, 7, 1);
-        // let vendor_index = texture_atlas.get_texture_index(vendor_handle).unwrap();
-        let atlas_handle = texture_atlases.add(texture_atlas_gabe);
-        
-        // set up a scene to display our texture atlas
-        commands
-        .spawn(Camera2dComponents::default())
-        //draw a sprite from the atlas
-        .spawn(SpriteSheetComponents {
-            scale: Scale(4.0),
-            // translation: Translation(Vec3::new(150.0, 0.0, 0.0)),
-            // sprite: TextureAtlasSprite::new(vendor_index as u32),
-            texture_atlas: atlas_handle,
-            ..Default::default()
-        })
-        .with(Timer::from_seconds(0.1))
-        .with(Gabe {
-            speed: 500.0,
-        });
-        //draw the atlas itself
-        // .spawn(SpriteComponents {
-        //     material: materials.add(texture_atlas_texture.into()),
-        //     translation: Vec3::new(-300.0, 0.0, 0.0).into(),
-        //     ..Default::default()
-        // });
-
         for ( gabe, mut translation, mut timer, mut sprite,  texture_atlas_handle) in &mut query.iter() {
             let mut direction = 0.0;
             if keyboard_input.pressed(KeyCode::Left) {
@@ -98,19 +57,37 @@ pub fn move_sprite_system(
             }
     
             if timer.finished
-                && (keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::Right))
+                && (keyboard_input.pressed(KeyCode::Left) && !keyboard_input.pressed(KeyCode::Right))
             {
                 let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
-                sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-    
+                let half_size = (texture_atlas.len() / 2) as u32;
+                if sprite.index < half_size || sprite.index == (texture_atlas.len()-1 as usize) as u32 {
+                  sprite.index = half_size;
+                  println!("1: sprite.index: {}", sprite.index);
+                }
+                sprite.index = (sprite.index + 1 )% texture_atlas.len() as u32;
+                println!("2: sprite.index: {}", sprite.index);
                 timer.reset();
             } else if !keyboard_input.pressed(KeyCode::Left) && !keyboard_input.pressed(KeyCode::Right)
             {
-                sprite.index = 0;
-            } else if !keyboard_input.pressed(KeyCode::Right) && !keyboard_input.pressed(KeyCode::Left)
+                if sprite.index < 7 {
+                    sprite.index = 0;
+                } else {
+                    sprite.index = 7;
+                }
+            } else if timer.finished && (keyboard_input.pressed(KeyCode::Right) && !keyboard_input.pressed(KeyCode::Left) )
             {
                 let texture_atlas = texture_atlases.get(&texture_atlas_handle).unwrap();
-                sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+                let half_size = (texture_atlas.len() / 2) as u32;
+                if sprite.index > half_size {
+                    sprite.index = 0;
+                }
+                sprite.index = (sprite.index + 1) % half_size;
+                timer.reset();
+            } else if keyboard_input.pressed(KeyCode::Space) {
+                // start animation lock for jump, but allow movement still in air, just more floaty? 
+                //if the user just jumped, other animations cannot run for entirety of airtime. this requires collision detection with the ground.
+                //We need a ground then!
             }
             // should have the texture decided for each button press.
             *translation.0.x_mut() += time.delta_seconds * direction * gabe.speed
@@ -119,12 +96,9 @@ pub fn move_sprite_system(
     
             // }
         }
-
-        rpg_sprite_handles.atlas_loaded = true;
-    }
-    
-   
 }
+    
+
 
 
 
@@ -137,19 +111,79 @@ fn setup2(
     let texture_handle = asset_server
         .load_sync(
             &mut textures,
-            "assets/textures/rpg/chars/gabe/gabe-idle-run.png",
+            "assets/textures/rpg/chars/gabe/gabe-idle-run-fix.png",
         )
         .unwrap();
+    // let texture_handle_ground = asset_server
+        // .load_sync(
+        //     &mut textures,
+        //     "assets/textures/groundTextures/groundTexture.png",
+        // )
+        // .unwrap();
+    let texture_handle_monster = asset_server
+        .load_sync(
+            &mut textures,
+            "assets/textures/monsters/monster.png",
+        )
+        .unwrap();
+    let texture_monster = textures.get(&texture_handle_monster).unwrap();
     let texture = textures.get(&texture_handle).unwrap();
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 7, 1);
+    // let texture_ground = textures.get(&texture_handle).unwrap();
+    let texture_atlas_monster = TextureAtlas::from_grid(texture_handle_monster, texture_monster.size, 17, 1);
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, texture.size, 14, 1);
+    // let texture_atlas_ground = TextureAtlas::from_grid(texture_handle_ground, texture_ground.size, 4, 1);
+    let texture_atlas_handle_monster = texture_atlases.add(texture_atlas_monster);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
-    commands
+    // let texture_atlas_handle_ground = texture_atlases.add(texture_atlas_ground);
+
+    let monster = commands
         .spawn(Camera2dComponents::default())
-        .spawn(SpriteSheetComponents {
-            texture_atlas: texture_atlas_handle,
-            scale: Scale(2.0),
+        .spawn( SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle_monster,
+            scale: Scale(5.0),
             ..Default::default()
         })
+        .with(Timer::from_seconds(0.05))
+        .with(Monster{ speed: 500.0 }).current_entity();
+        
+        let gabe = commands.spawn(SpriteSheetComponents {
+            texture_atlas: texture_atlas_handle,
+            scale: Scale(8.0),
+            ..Default::default()
+        })
+        
         .with(Timer::from_seconds(0.1))
-        .with(Gabe { speed: 500.0 });
-}
+        .with(Gabe { speed: 500.0 }).current_entity();
+        // .spawn(SpriteSheetComponents {
+        //     texture_atlas: texture_atlas_handle_ground,
+        //     scale:Scale(2.0), 
+        //     ..Default::default()
+        // });
+
+    }
+
+
+
+    //planning
+
+    //components
+    struct Player {
+        
+    }
+
+    struct UnitType {
+
+    }
+
+    
+
+    //setup scene like so:
+
+    fn setup4(
+        mut commands: Commands,
+        asset_server: Res<AssetServer>,
+        mut textures: ResMut<Assets<Texture>>,
+        mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    ) {
+
+    }
